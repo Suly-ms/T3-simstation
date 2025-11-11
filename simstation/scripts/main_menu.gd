6c0b93cd2cda1d180f22dc81b6745b554f73258d
ResyYT <67598309+ResyYT@users.noreply.github.com>
2025-11-11 20:20:51 +0100

    création de la branche et suppression du projet godot

--- simstation/scripts/main_menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_btn_jouer_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/play.tscn")
-
-
-func _on_btn_quitter_pressed() -> void:
-	get_tree().quit()
--- simstation/scripts/night_mode.gd
+++
@@ -1,5 +0,0 @@
-extends CanvasLayer
-
-
-func _on_night_mode_pressed() -> void:
-	self.visible = not self.visible
