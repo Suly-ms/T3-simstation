f222aa01ba9ab1fcdb0a8a591a8a2834ba645c40
Erm5s <emre.sen@iutrs.unistra.fr>
2025-12-11 01:52:08 +0100

    Modification main menu v1

--- simstation/Model/main_menu.gd
+++ simstation/Model/main_menu.gd
@@ -4,6 +4,9 @@ extends Control
 func _on_btn_jouer_pressed() -> void:
 	get_tree().change_scene_to_file("res://View/play.tscn")
 
+func _on_btn_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://View/play.tscn")
+	get_tree().change_scene_to_file("res://View/options.tscn")
 
 func _on_btn_quitter_pressed() -> void:
 	get_tree().quit()
