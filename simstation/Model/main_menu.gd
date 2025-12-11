a5d0fb148cb3e7dbcda1f5ae2295ef44ec8236fc
Erm5s <emre.sen@iutrs.unistra.fr>
2025-12-12 00:20:31 +0100

    Options accessible depuis le main menu + tout responsive

--- simstation/Model/main_menu.gd
+++ simstation/Model/main_menu.gd
@@ -1,12 +1,18 @@
 extends Control
 
+const MENU_OPTIONS_SCENE = preload("res://View/options.tscn")
+var menu_instance = null
 
 func _on_btn_jouer_pressed() -> void:
 	get_tree().change_scene_to_file("res://View/play.tscn")
 
 func _on_btn_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://View/play.tscn")
-	get_tree().change_scene_to_file("res://View/options.tscn")
+	if menu_instance == null:
+		menu_instance = MENU_OPTIONS_SCENE.instantiate()
+		get_parent().add_child(menu_instance) 
+	else:
+		var is_visibles = menu_instance.visible
+		menu_instance.visible = not is_visibles
 
 func _on_btn_quitter_pressed() -> void:
 	get_tree().quit()
